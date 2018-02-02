f966e3b8e69470465afc9e015c5e80c12b66d009
Aleh Isakau <isakaualeh@gmail.com>
2018-02-02 17:50:33 +0300

    working on unit tests

--- test/unit/specs/utils.js
+++ test/unit/specs/utils.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 
-export const getVueEl = (Component, props) => {
+const getContext = (Component, props = {}, data = {}) => {
   const Ctor = Vue.extend(Component)
-  const vm = new Ctor({ propsData: props }).$mount()
 
-  return vm.$el
+  return new Ctor({ propsData: props, data }).$mount()
+}
+
+export const getVueInstance = (Component, props) => {
+  return getContext(Component, props)
+}
+
+export const getVueEl = (Component, props) => {
+  return getVueInstance(Component, props).$el
 }
